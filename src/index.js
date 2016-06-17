8271a7a02c2bcb6782cfc5378801c0abfc5db5f0
Micheal Harker <micheal@michealharker.com>
2016-06-17 21:58:52 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import dude from 'debug-dude'
+const { /*debug,*/ log, info /*, warn, error*/ } = dude('bot')
+
+import { version } from '../package.json'
+info(`coffea-starter bot v${version} starting`)
+
+import config from '../config.json'
+
+import { connect, message } from 'coffea'
+const networks = connect(config)
+
+// --
+
+networks.on('message', (evt, reply) => {
+  log('Received message event: %o', evt)
+
+  // TODO: do something with messages here or remove the message event handler
+})
+
+networks.on('command', (evt, reply) => {
+  log('Received command event: %o', evt)
+
+  switch (evt.cmd) {
+    case 'say':
+      reply(message(evt.channel, evt.args.join(' ')))
+      break
+    // TODO: add more commands here
+  }
+})
+
+// TODO: write more code here or adjust code above
